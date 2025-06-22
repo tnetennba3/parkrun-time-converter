d4ff521fce8e201efd68b677a7f0ff48f328d101
Helena Thompson <helenathompson.dev@gmail.com>
2025-06-22 15:33:35 +0100

    fix: Jest setup

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  testEnvironment: "jsdom",
+};
+
+export default createJestConfig(config);
